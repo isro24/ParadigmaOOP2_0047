5d41479c152ca86c7c489d6af8c989efe2ec419c
isro24 <alamat_email_anda@example.com>
2024-05-27 15:41:45 +0700

    membuat class dengan acc mod private untuk x y dan public untuk setxy dan menampilkannya

---
+++ abstarksi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas{
+private:
+    string x, y;
+public:
+    // metdho untuk mengisi nilai
+    //private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai
+    void display(){
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
