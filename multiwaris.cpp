78bace4759e9b1454ac295e1e3af36a74f0fe656
isro24 <alamat_email_anda@example.com>
2024-05-27 15:08:50 +0700

    membaut class budi dengan pekerja dan pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -20,8 +20,17 @@ public:
 };
 
 class pelajar : virtual public orang {
+public:
     pelajar(int pUmur) : orang(pUmur)
     {
         cout << "Pelajar dibuat\n" << endl;
+    }
+};
+
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) : orang(pUmur), pekerja(pUmur), pelajar(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
     }
 };
